04c585eb42f57de3f024ac3a4c50f8680de9d2d0
DarushDev <darushfaqiri@gmail.com>
2017-12-27 22:00:15 +0800

    Add new component routes to index.js router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import ViewContact from '@/components/ViewContact'
+import NewContact from '@/components/NewContact'
 
 Vue.use(Router)
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/add',
+      name: 'new-contact',
+      component: NewContact
+    },
+    {
+      path: '/:person',
+      name: 'view-contact',
+      component: ViewContact
     }
   ]
 })
