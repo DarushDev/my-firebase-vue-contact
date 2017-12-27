51aedac41160ebc50ceee42e1aad5a914feb92d3
DarushDev <darushfaqiri@gmail.com>
2017-12-28 02:35:43 +0800

    Create new contact form

---
+++ src/components/NewContact.vue
@@ -0,0 +1,87 @@
+<template>
+  <section class="container">
+    <h1>Add New Contact</h1>
+
+    <form @submit.prevent="saveContact">
+
+      <div class="field">
+        <label for="" class="label">First Name</label>
+        <div class="control">
+          <input type="text" class="input" placeholder="First Name" v-model="firstname" required>
+        </div>
+      </div>
+
+      <div class="field">
+        <label for="" class="label">Last Name</label>
+        <div class="control">
+          <input type="text" class="input" placeholder="Last Name" v-model="lastname" required>
+        </div>
+      </div>
+
+      <div class="field">
+        <label for="" class="label">Email Address</label>
+        <div class="control">
+          <input type="email" class="input" placeholder="Email Address" v-model="emailaddress" required>
+        </div>
+      </div>
+
+      <div class="field">
+        <label for="" class="label">Phone Number</label>
+        <div class="control">
+          <input type="text" class="input" placeholder="Phone Number" v-model="phonennumber" required>
+        </div>
+      </div>
+
+      <div class="field">
+        <div class="control">
+          <button type="submit" class="button is-link">Submit</button>
+        </div>
+      </div>
+
+    </form>
+  </section>
+
+</template>
+
+<script>
+  import db from '../../config/firebaseInit'
+  export default {
+    name: 'new-contact',
+    data () {
+      return {
+        firstname: null,
+        lastname: null,
+        emailaddress: null,
+        phonenumber: null
+      }
+    },
+    methods: {
+      saveContact () {
+        db.collection('contacts').add({
+          firstname: this.firstname,
+          lastname: this.lastname,
+          emailaddress: this.emailaddress,
+          phonenumber: this.phonenumber,
+          slug: this.generateUUID()
+        })
+          .then(function (docRef) {
+            console.log('Document written with ID: ', docRef.id)
+          })
+          .catch(function (error) {
+            console.error('Error adding document: ', error)
+          })
+      },
+      generateUUID () {
+        let date = new Date().getTime()
+        let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+          let random = (date + Math.random() * 16) % 16 | 0
+          date = Math.floor(date / 16)
+          return (c === 'x' ? random : (random & 0x3 | 0x8)).toString(16)
+        })
+        return uuid
+      }
+    }
+  }
+</script>
+
+<styl></styl>
